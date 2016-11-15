ca9bd1809bbadf35bac2a891c404bba7789ca98e
yangx066 <yangx066@gmail.com>
2016-11-15 10:34:06 -0500

    hw11

---
+++ w11/w11/Assets/scripts/cameracontroller.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameracontroller : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//For follow camera,procedural animations,etc.,it's best to use dateUpdate
+	//lateUpdate,runs after all items have been processed
+
+	void LateUpdate() {
+		transform.position = player.transform.position + offset;
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pike Up")) {
+			other.gameObject.SetActive (false);
+		}
+	}
+}
